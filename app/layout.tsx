80916cc5d48eea7175816fc55339c1fc21529e67
Germán Aliprandi <galiprandi@gmail.com>
2025-04-06 18:05:48 -0300

    Refactor page params type and remove unused font imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
 import AsideBar from "@/components/AsideBar";
 import styles from "./layout.module.css";
 import { siteConfig } from "@/config";
