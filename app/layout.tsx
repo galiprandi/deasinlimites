77ee1fc11185c277eb666e95cd82754e383c67cd
Germán Aliprandi <galiprandi@gmail.com>
2025-04-12 20:54:38 -0300

    Update social media image URL from logo.png to logo_social.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: siteConfig.title,
     images: [
       {
-        url: "/logo.png",
+        url: "/logo_social.png",
         width: 1200,
         height: 630,
         alt: siteConfig.title,
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     images: {
-      url: "/logo.png",
+      url: "/logo_social.png",
       width: 1200,
       height: 630,
       alt: siteConfig.title,
